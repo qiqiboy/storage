d07d32bd5acaa09638810dad9a14b99c286932e8
qiqiboy <imqiqiboy@gmail.com>
2013-12-04 14:00:30 +0800

    update 2.0

--- README.md
+++ README.md
@@ -3,21 +3,25 @@ storage
 
 > 提供LocalStorage的本地化存储方案，支持IE（通过IE特性userData）。
 
-注意：IE6 IE7等是通过userData来持久化存储数据的，为了可以达到获取本域下所有已存储数据的目的，所以所有数据都是存储在一个xml文件里的。因此一个域下不能存储太大的数据，限制为128KB（受限站点64KB）。
-另外不支持复杂对象的存储，例如Array、Object等，如果有此需求，请转为JSON后再进行存储。
+注意：IE6 IE7等是通过userData来持久化存储数据的，不支持复杂对象的存储，例如Array、Object等，如果有此需求，请转为JSON后再进行存储。
 
 ## 如何使用
 ```javascript
 // 首先在页面中引入storage.js
-
+//可以通过两种方式调用：
+//一是直接调用
 storage.get(key); //获取名字为 key 的storage的值
-
 storage.set(key,value); //添加或修改一个名字为 key， 值为 value 的storage，true表示设置成功，false表示设置失败
-
 storage.remove(key,value); //删除名字为 key 的storage，true表示删除成功，false表示删除失败
-
 storage.clear(); //清除所有的storage
-
 storage.size(); //返回已存储的storage数量
+
+//二是通过先获取一个实例引用，此种方式可以避开单个xml只能存储64KB数据的限制。但是请注意的是，一个域下最多支持10个，所以一个页面中请避免超过10个storage实例。
+var mystorage=storage('myName');
+mystorage.get(key); //获取名字为 key 的storage的值
+mystorage.set(key,value); //添加或修改一个名字为 key， 值为 value 的storage，true表示设置成功，false表示设置失败
+mystorage.remove(key,value); //删除名字为 key 的storage，true表示删除成功，false表示删除失败
+mystorage.clear(); //清除所有的storage
+mystorage.size(); //返回已存储的storage数量
 
 ````
---
+++ src/storage.js
@@ -0,0 +1,177 @@
+/**
+ * storage v2.0
+ * By qiqiboy, http://www.qiqiboy.com, http://weibo.com/qiqiboy, 2013/12/03
+ */
+
+var storage=(function(ROOT, Struct, undefined){
+	"use strict";
+	
+	var doc=ROOT.document,
+		storage=ROOT.localStorage || ROOT.globalStorage&&ROOT.globalStorage[location.hostname];
+	
+	Struct.prototype=Struct;
+	
+	var getStorage=function(name){
+		return ( storage ? {
+			length: 0,
+			storages: [],
+			init:function(){
+				this.name=name?name+'/':'';
+				this.getStorages();
+				return this;
+			},
+			getStorages:function(){
+				var storages=this.storages=[],
+					i=0,j=storage.length,key,_key,
+					testReg=new RegExp('^'+this.name.replace(/([\.\?\+\*/])/g,"\\$1"),'i');
+				for(;i<j;i++){
+					key=storage.key(i);
+					if(testReg.test(key)){
+						_key=key.replace(testReg,'');
+						storages.push(_key);
+						storages[_key]=storage.getItem(key);
+					}
+				}
+				this.length=storages.length;
+				return this;
+			},
+			key:function(i){
+				return this.storages[i];
+			},
+			getItem:function(key){
+				return this.storages[key];
+			},
+			setItem:function(key,value){
+				storage.setItem(this.name+key,value);
+				return this.getStorages();
+			},
+			removeItem:function(key){
+				storage.removeItem(this.name+key);
+				return this.getStorages();
+			},
+			clear:function(){
+				if(this.name){
+					var len=this.length,
+						i=0;
+					while(i++<len){
+						this.removeItem(this.key(0));
+					}
+				}else{
+					storage.clear();
+				}
+				return this;
+			}
+		} : {
+				length:0,
+				userData:doc.getElementsByTagName('head')[0],
+				init:function(){
+					var userData=this.userData;
+					this.name=name?'_'+name:'';
+					try{
+						if(!Struct.loaded){
+							userData.addBehavior("#default#userdata");
+							Struct.loaded=true;
+						}
+						this.refresh();
+					}catch(e){}
+					
+					return this;
+				},
+				load:function(){
+					try{
+						this.userData.load("oXMLBranch"+this.name);
+						this.storeNode=this.userData.xmlDocument.firstChild;
+					}catch(e){}
+				},
+				refresh:function(){
+					this.load();
+					this.length=this.storeNode.attributes.length;
+					return this;
+				},
+				key:function(i){
+					this.load();
+					return this.storeNode.attributes[i].nodeName;
+				},
+				getItem:function(key){
+					this.load();
+					return this.userData.getAttribute(key);
+				},
+				setItem:function(key,value){
+					this.load();
+					this.userData.setAttribute(key,value);
+					this.save();
+				},
+				removeItem:function(key){
+					this.load();
+					this.userData.removeAttribute(key);
+					this.save();;
+				},
+				clear:function(){
+					this.load();
+					this.userData.xmlDocument.removeChild(this.storeNode);
+					this.save();
+				},
+				save:function(){
+					this.userData.save("oXMLBranch"+this.name);
+					this.refresh();
+				}
+			} ).init();
+	}
+	
+	var prop, fn={
+		version:"2.0",
+		constructor:Struct,
+		init:function(name){
+			this.storage=getStorage(name||'');
+			return this.refresh();
+		},
+		refresh:function(){
+			this.storages=this.getStorages();
+			return this;
+		},
+		has:function(key){
+			return this.get(key)!=null;
+		},
+		get:function(key){
+			return this.storages[key];
+		},
+		set:function(key,value){
+			this.storage.setItem(key,value);
+			return this.refresh().has(key);
+		},
+		remove:function(key){
+			this.storage.removeItem(key);
+			return !this.refresh().has(key);
+		},
+		clear:function(){
+			this.storage.clear();
+			return this.refresh();
+		},
+		size:function(){
+			return this.storage.length;
+		},
+		getStorages:function(){
+			var storages={},
+				storage=this.storage,
+				i=0,
+				len=storage && storage.length || 0,
+				key;
+			for(;i<len;i++){
+				key=storage.key(i);
+				storages[key]=storage.getItem(key);
+			}
+			return storages;
+		}
+	}
+	
+	for(prop in fn){
+		Struct[prop]=fn[prop];
+	}
+	
+	return Struct.init();
+})(window, function(name){
+	if(!(this instanceof arguments.callee)){
+		return new arguments.callee(name);
+	}
+	this.init(name);
+});
